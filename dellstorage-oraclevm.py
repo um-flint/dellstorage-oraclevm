7379fddc7fef58c5d43bfb93ecf0e2165df066c7
Robinson, Adam <adarobin@umflint.edu>
2016-05-06 22:40:13 -0400

    display the names from both systems for all compellent disks

--- dellstorage-oraclevm.py
+++ dellstorage-oraclevm.py
@@ -36,9 +36,27 @@ def main():
     check_manager_state(ovmUri,ovms)
 
     dellauth = {'Authorization': 'Basic ' + base64.b64encode(dellusername + ':' + dellpassword), 'Content-Type': 'application/json', 'x-dell-api-version': '2.2'}
-    r=s.post(dellUri+'/ApiConnection/Login','{}',headers=dellauth)
-         
-    r=s.post(dellUri+'/ApiConnection/Logout','{}')
+    r=dells.post(dellUri+'/ApiConnection/Login','{}',headers=dellauth)
+
+    r=dells.get(dellUri+'/StorageCenter/ScVolume')
+    delldisks=r.json()
+    
+    r=ovms.get(ovmUri+'/StorageElement')
+    ovmdisks=r.json()
+    for disk in ovmdisks:
+        if disk['vendor'] == 'COMPELNT':
+            for delldisk in delldisks:
+                #Oracle VM puts the Page 83 Type in front of the actual identifier
+                #so cut off the first character
+                if disk['page83Id'][1:] == delldisk['deviceId']:
+                    print 'Disk with Page 83 Id ' + delldisk['deviceId']
+                    print 'Oracle VM  name is ' + disk['name']
+                    print 'Compellent name is ' + delldisk['name']
+                    print
+#            print json.dumps(disk, indent=4, sort_keys=True)
+
+
+    r=dells.post(dellUri+'/ApiConnection/Logout','{}')
 
     return
 
